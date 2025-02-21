590187ba5cd7d60146add4bffb24872adcd7c142
𝓻𝓮𝔁𝓮𝓻 <rexerwong@gmail.com>
2025-02-21 18:01:20 +0800

    feat(react): optimize hooks to `useChannelState` & `useChannelStateWithSelector`

--- src/react.ts
+++ src/react.ts
@@ -1,20 +1,33 @@
 import { type Draft, produce } from 'immer'
-import { useDebugValue } from 'react'
+import { useDebugValue, useSyncExternalStore } from 'react'
 import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector'
 import type { ChannelStateApi } from './index'
 import type { MaybePartial } from './vanilla'
 
-export type Return<T, U> = readonly [
+export type Return<T, U = T> = readonly [
   state: U,
   setState: (nextStateOrUpdater: MaybePartial<T> | ((draft: Draft<T>) => void), replace?: boolean) => void,
 ]
 
-export function useChannelState<T, U = T>(
+export function useChannelState<T>(api: ChannelStateApi<T>): Return<T> {
+  const state = useSyncExternalStore(api.subscribe, api.getState, api.getInitialState)
+  const setState: Return<T>[1] = (nextStateOrUpdater, replace) => {
+    const nextState =
+      typeof nextStateOrUpdater === 'function' ? produce(api.getState(), nextStateOrUpdater as any) : nextStateOrUpdater
+    api.setState(nextState, replace)
+  }
+
+  useDebugValue(state)
+
+  return [state, setState]
+}
+
+export function useChannelStateWithSelector<T, U = T>(
   api: ChannelStateApi<T>,
   selector: (state: T) => U = (v: any) => v,
   equalityFn?: (a: U, b: U) => boolean,
 ): Return<T, U> {
-  const state = useSyncExternalStoreWithSelector(api.subscribe, api.getState, undefined, selector, equalityFn)
+  const state = useSyncExternalStoreWithSelector(api.subscribe, api.getState, api.getInitialState, selector, equalityFn)
   const setState: Return<T, U>[1] = (nextStateOrUpdater, replace) => {
     const nextState =
       typeof nextStateOrUpdater === 'function' ? produce(api.getState(), nextStateOrUpdater as any) : nextStateOrUpdater
